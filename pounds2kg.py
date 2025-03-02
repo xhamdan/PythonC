7ce1dd80beb4b20038dd3ba1c2d73f622d6b60dc
xhamdan <magezi.sh@gmail.com>
2025-03-02 13:41:42 +0300

    convert formulae

--- pounds2kg.py
+++ pounds2kg.py
@@ -1,4 +1,9 @@
 weight = input("Please Enter your weight: ")
 unit   = input('(L)bs or (K)g: ')
+if unit.upper()=="L":
+    converted= weight * 0.45
+print(f "You are {converted} Kilos")
+else:
+    converted = weight / 0.45
 
 
