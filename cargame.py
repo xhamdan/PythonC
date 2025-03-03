ea10d24a3c248f369c155b62b01c6b2c468e8442
xhamdan <magezi.sh@gmail.com>
2025-03-03 14:55:52 +0300

    test-started

--- cargame.py
+++ cargame.py
@@ -1,18 +1,23 @@
 command=""
+started = False
 while True:
     command = input(">").lower()
     if command == "start":
+        if started:
+        print("Car Already Started!")
+    else:
+        started = True
         print("Car is started...")
-    elif command == "Stop":
+    elif command == "stop":
         print("Car is Stopped")
     elif command == "help":
         print("""
         Start - To Start the Car
         Stop  - To Stop the Car
-        Quite - To Quite
+        Quite - To Quit
         
         """)
-    elif command =="quite":
+    elif command =="quit":
         break
 else:
     print("Oops - I do not understand this")
